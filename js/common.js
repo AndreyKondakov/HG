3ff35816d4f1b90440bfdc1881af37b9bf59ca5f
Andrey <primax7k@gmail.com>
2017-10-03 15:08:57 +0300

    Minor changes

--- js/common.js
+++ js/common.js
@@ -1,6 +1,18 @@
 //=======================  tabs  =======================
 $(".tab_item").not(":first").hide();
-$(".inf-tab .tab").click(function() {
-    $(".inf-tab .tab").removeClass("active").eq($(this).index()).addClass("active");
-    $(".tab_item").hide().eq($(this).index()).fadeIn()
+$(".inf-tab .tab").click(function () {
+  $(".inf-tab .tab").removeClass("active").eq($(this).index()).addClass("active");
+  $(".tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
+
+
+// $(document).ready(function () {
+//   $('#nav li').hover(
+//     function () {
+//       $('ul', this).slideDown(300);
+//     },
+//     function () {
+//       $('ul', this).slideUp(100);
+//     }
+//   );
+// });
