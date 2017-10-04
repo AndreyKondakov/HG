76ab4330dc1e280e4ea76f23d66680e4da81ecc9
Andrey <primax7k@gmail.com>
2017-10-04 12:38:03 +0300

    Updating: pages( right, user bars ), style

--- js/common.js
+++ js/common.js
@@ -4,14 +4,3 @@ $(".inf-tab .tab").click(function () {
     $(".inf-tab .tab").removeClass("active").eq($(this).index()).addClass("active");
     $(".tab_item").hide().eq($(this).index()).fadeIn()
 }).eq(0).addClass("active");
-
-// nav-right
-/*
-$(document).ready(function () {
-  $('.nav-lang li').hover(
-    function () {
-      $('ul', this).slideToggle(300);
-    }
-
-  );
-});*/
