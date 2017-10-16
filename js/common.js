cd3ea51328b92cdf6dd20c5fe1a7d37bfb082439
AndreyKondakov <primax7k@gmail.com>
2017-10-16 17:49:17 +0300

    Add gallery for product

--- js/common.js
+++ js/common.js
@@ -11,3 +11,14 @@ $(document).ready(function(){
     $("#panel").slideToggle("slow");
   });
 });
+
+// gallery for product
+$(document).ready(function(){
+  $("h2").append('<em></em>')
+  $(".thumbs a").click(function(){
+    var largePath = $(this).attr("href");
+    var largeAlt = $(this).attr("title");
+    $("#largeImg").attr({ src: largePath, alt: largeAlt });
+    $("h2 em").html(" (" + largeAlt + ")"); return false;
+  });
+});
